bb174a5be7405220be5c104822253192da7f32a3
STRONGSHARK <woghfghf@naver.com>
2022-12-09 02:52:11 +0900

    useRef learning(재수정)#1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState, useRef, useEffect } from "react";
+
 function App() {
+  const [count, setCount] = useState(1);
+  const renderCount = useRef(1);
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+    console.log(`렌더 : ${renderCount.current}`);
+  });
   return (
     <div>
-      hello world
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>btn</button>
     </div>
   );
 }
